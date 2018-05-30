17b5d5efb435e83873e73e87fb975727a0392bd4
brojas <Brian-Rojas>
2018-05-30 00:58:44 -0700

    Added accounts and base html template and accounts url, also started programming the account views for signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    # TODO add redirect to homepage and logout
+    return render(request, 'accounts/signup.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                context = {
+                    'error': 'Username has already been taken.'
+                }
+                return render(request, 'accounts/signup.html', context)
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=[request.POST['password']])
+                auth.login(request, user)
+                return redirect('home')
+
+        else:
+            context = {
+                'error': 'Passwords must match.'
+            }
+            return render(request, 'accounts/signup.html', context)
+        # The user wants to sign up
+    else:
+        return render(request, 'accounts/signup.html')
